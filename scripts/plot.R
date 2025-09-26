0e03ea606e0499280f74e481ca8929dfb904ce5d
João Gabriel <95772104+jgluiggi@users.noreply.github.com>
2025-09-26 23:28:28 +0000

    Delete scripts directory

--- scripts/plot.R
+++
@@ -1,9 +0,0 @@
-# Se o pacote não tiver instalado na máquina.
-#install.packages('ggplot2', repos = "http://cran.us.r-project.org")
-
-library('ggplot2')
-
-args <- commandArgs()
-
-data = read.table(args[length(args)], header = T)
-ggplot(data, aes(x = sample, y = time, colour = alg)) + geom_line()
